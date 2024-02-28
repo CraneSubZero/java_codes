70d90f61ccd2796481b2ae6f9f9db0fd1c8e74f4
unknown <destroyer3421@gmail.com>
2024-02-28 14:02:38 +0800

    corrected the payroll

--- payrollcalculator/PayrollCalculator.java
+++ payrollcalculator/PayrollCalculator.java
@@ -1,4 +1,4 @@
-import java.util.Scanner; // Flagged for Prelim Second Sem
+import java.util.Scanner;
 
 public class PayrollCalculator {
 
@@ -6,48 +6,43 @@ public class PayrollCalculator {
 
         Scanner sc = new Scanner(System.in);
 
-        // Initiate the Do While Statement.
-        do { // This is the False Path.
-            try { // Enable Try Catch
+        do {
+            try {
 
-                String name; // Always input both a to e zero.
+                String name;
                 double workrate = 0;
                 double numberofdays = 0;
                 double mediacare = 0;
                 double advances = 0;
                 double incometaxes = 0;
 
-                // Request five integer inputs from the user
                 System.out.print("Enter Employee Name: ");
-                String name = scanner.nextLine();
+                name = sc.nextLine(); // Fix variable name and use 'sc' instead of 'scanner'
                 System.out.print("Enter Daily Work Rate: ");
-                workrate = sc.nextInt();
+                workrate = sc.nextDouble();
                 System.out.print("Enter Number of Days Worked: ");
-                numberofdays = sc.nextInt();
-                System.out.print("Enter Enter Mediacare deduction: ");
-                mediacare = sc.nextInt();
+                numberofdays = sc.nextDouble();
+                System.out.print("Enter Mediacare deduction: ");
+                mediacare = sc.nextDouble();
                 System.out.print("Enter Advances deduction: ");
-                advances = sc.nextInt();
+                advances = sc.nextDouble();
                 System.out.print("Enter income taxes deduction: ");
-                incometaxes = sc.nextInt();
-
+                incometaxes = sc.nextDouble();
 
                 double productResult = grosspay(workrate, numberofdays);
                 double sumResult = totaldeductionamount(mediacare, advances, incometaxes);
-                double differenceResult = netpay(grosspay, totaldeductionamount);
+                double differenceResult = netpay(productResult, sumResult);
 
-                System.out.println("Employee Name: " name);
-                System.out.println("Gross Pay: " productResult);
-                System.out.println("Total Deduction: " sumResult);
-                System.out.println("Net Pay: " differenceResult);
+                System.out.println("Employee Name: " + name);
+                System.out.println("Gross Pay: " + productResult);
+                System.out.println("Total Deduction: " + sumResult);
+                System.out.println("Net Pay: " + differenceResult);
 
-                // Ask the user if they want to go again
                 System.out.print("Do you want to go again? (Y/N): ");
                 String goAgain = sc.next();
 
-                // Use if-else statement
                 if (goAgain.equalsIgnoreCase("N")) {
-                    break; // Exit the loop if the user does not want to go again
+                    break;
                 } else if (!goAgain.equalsIgnoreCase("Y")) {
                     throw new IllegalArgumentException("Invalid input. Please enter 'Y' or 'N'.");
                 }
@@ -59,24 +54,22 @@ public class PayrollCalculator {
                     System.out.println("Invalid input. Please Enter Numeric Values");
                 }
             } finally {
-                sc.nextLine(); // Close the invalid input to avoid an infinite loop within your statements.
+                sc.nextLine();
             }
-        } while (true); // This is the True Path.
-    sc.close();
-    } 
+        } while (true);
 
-    
+        sc.close();
+    }
+
     public static double grosspay(double workrate, double numberofdays) {
         return workrate * numberofdays;
     }
 
-    
     public static double totaldeductionamount(double mediacare, double advances, double incometaxes) {
         return mediacare + advances + incometaxes;
     }
 
-    
     public static double netpay(double grosspay, double totaldeductionamount) {
         return grosspay - totaldeductionamount;
     }
-}       
+}
