d9bfc8dc62042f09ec8239debf6aa4d9c10d39fe
BERBENZANA <120699750@comlabserver.csabcomlab.edu.ph>
2024-03-04 10:34:35 +0800

    Added some Comments in Parameter Passing

--- parameterpassing/parameterpassing.java
+++ parameterpassing/parameterpassing.java
@@ -1,17 +1,17 @@
 public class parameterpassing {
 
 	public static void main(String[] args) {
-		int a = 30;
-		int b = 45;
+		int a = 420;
+		int b = 900;
 		System.out.println("Before swapping, a = " + a + " and b = " + b);
 
 		// Invoke the swap method
 		swapFunction(a, b);
-		System.out.println("\n**Now, Before and After swapping values will be same here**:");
+		System.out.println("\n**Now, Before and After swapping values will be same here**:"); // The "\n" function serves as an "enter" button to skip the line the terminus.
 		System.out.println("After swapping, a = " + a + " and b is " + b);	
 	}
 
-	public static void swapFunction(int a, int b) {
+	public static void swapFunction(int a, int b) { // Since its void, there will be no Return Value.
 		System.out.println("before swapping(Inside), a = " + a + " b = " + b);
 
 		// Swap n1 with n2
