d4d45fbd92a7ced0316a8297414571b90b24042b
BERBENZANA <120699750@comlabserver.csabcomlab.edu.ph>
2024-03-18 10:20:38 +0800

    added some comment in discussion

--- discussionjavamethods/discussion.java
+++ discussionjavamethods/discussion.java
@@ -6,12 +6,12 @@ public class discussion {
     System.out.print("Enter your full name: ");
     String fullName = scan.nextLine();
 
-    int firstSpaceIndex = fullName.indexOf(" ");
-    int secondSpaceIndex = fullName.indexOf(" ", firstSpaceIndex + 1); // Find the index of the second space
+    int firstSpaceIndex = fullName.indexOf(" "); // For the First Name
+    int secondSpaceIndex = fullName.indexOf(" ", firstSpaceIndex + 1); // Must Find the index of the second name
 
-    String firstName = fullName.substring(0, firstSpaceIndex);
-    String secondName = fullName.substring(firstSpaceIndex + 1, secondSpaceIndex); // Extract the second name
-    String lastName = fullName.substring(secondSpaceIndex + 1);
+    String firstName = fullName.substring(0, firstSpaceIndex); // here is for the first name
+    String secondName = fullName.substring(firstSpaceIndex + 1, secondSpaceIndex); // here is for the second name
+    String lastName = fullName.substring(secondSpaceIndex + 1); // here is for the last name
 
     System.out.println("First name: " + firstName);
     System.out.println("Second name: " + secondName);
