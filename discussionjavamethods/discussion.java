eca4394034359ca8beb1698a22a09cc83b30f46e
BERBENZANA <120699750@comlabserver.csabcomlab.edu.ph>
2024-03-20 10:20:47 +0800

    added adjustments on the discussion.java

--- discussionjavamethods/discussion.java
+++ discussionjavamethods/discussion.java
@@ -8,13 +8,16 @@ public class discussion {
 
     int firstSpaceIndex = fullName.indexOf(" "); // For the First Name
     int secondSpaceIndex = fullName.indexOf(" ", firstSpaceIndex + 1); // Must Find the index of the second name
+    int thirdSpaceIndex = fullName.indexOf(" ", secondSpaceIndex + 1); // Must Find the index of the third name
 
     String firstName = fullName.substring(0, firstSpaceIndex); // here is for the first name
     String secondName = fullName.substring(firstSpaceIndex + 1, secondSpaceIndex); // here is for the second name
-    String lastName = fullName.substring(secondSpaceIndex + 1); // here is for the last name
+    String thirdName = fullName.substring(secondSpaceIndex + 1, thirdSpaceIndex); // here is for the middle name
+    String lastName = fullName.substring(thirdSpaceIndex + 1); // here is for the last name
 
     System.out.println("First name: " + firstName);
     System.out.println("Second name: " + secondName);
+    System.out.println("Middle name: " + thirdName);
     System.out.println("Last name: " + lastName);
   }
 }
