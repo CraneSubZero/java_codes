0a2d58d709a4f79bb258f124fd9fe61946ae4ddc
CraneSubZero <destroyer3421@gmail.com>
2024-03-24 01:16:39 +0800

    added some try catch on discussion.java and samplearray.java

--- discussionjavamethods/discussion.java
+++ discussionjavamethods/discussion.java
@@ -1,23 +1,27 @@
 import java.util.Scanner;
 
 public class discussion {
-  public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
-    System.out.print("Enter your full name: ");
-    String fullName = scan.nextLine();
+    public static void main(String[] args) {
+        try {
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Enter your full name: ");
+            String fullName = scan.nextLine();
 
-    int firstSpaceIndex = fullName.indexOf(" "); // For the First Name
-    int secondSpaceIndex = fullName.indexOf(" ", firstSpaceIndex + 1); // Must Find the index of the second name
-    int thirdSpaceIndex = fullName.indexOf(" ", secondSpaceIndex + 1); // Must Find the index of the third name
+            int firstSpaceIndex = fullName.indexOf(" "); // For the First Name
+            int secondSpaceIndex = fullName.indexOf(" ", firstSpaceIndex + 1); // Must Find the index of the second name
+            int thirdSpaceIndex = fullName.indexOf(" ", secondSpaceIndex + 1); // Must Find the index of the third name
 
-    String firstName = fullName.substring(0, firstSpaceIndex); // here is for the first name
-    String secondName = fullName.substring(firstSpaceIndex + 1, secondSpaceIndex); // here is for the second name
-    String thirdName = fullName.substring(secondSpaceIndex + 1, thirdSpaceIndex); // here is for the middle name
-    String lastName = fullName.substring(thirdSpaceIndex + 1); // here is for the last name
+            String firstName = fullName.substring(0, firstSpaceIndex); // here is for the first name
+            String secondName = fullName.substring(firstSpaceIndex + 1, secondSpaceIndex); // here is for the second name
+            String middleName = fullName.substring(secondSpaceIndex + 1, thirdSpaceIndex); // here is for the middle name
+            String lastName = fullName.substring(thirdSpaceIndex + 1); // here is for the last name
 
-    System.out.println("First name: " + firstName);
-    System.out.println("Second name: " + secondName);
-    System.out.println("Middle name: " + thirdName);
-    System.out.println("Last name: " + lastName);
-  }
+            System.out.println("First name: " + firstName);
+            System.out.println("Second name: " + secondName);
+            System.out.println("Middle name: " + middleName);
+            System.out.println("Last name: " + lastName);
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("Error: Invalid input format. Please enter your full name in the correct format.");
+        }
+    }
 }
--- samplearray/samplearray.java
+++ samplearray/samplearray.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class samplearray {
@@ -9,18 +10,22 @@ public class samplearray {
         // This is the part where you tell the user to prompt the Integers that it wants.
         System.out.println("Enter 5 integers:");
 
-        // Loops 5 times
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.print("\nInteger " + (i + 1) + ": ");
-            numbers[i] = scan.nextInt();
-            sum += numbers[i];
+        try {
+            // Loops 5 times
+            for (int i = 0; i < numbers.length; i++) {
+                System.out.print("\nInteger " + (i + 1) + ": ");
+                numbers[i] = scan.nextInt();
+                sum += numbers[i];
+            }
+
+            // This is for the average, you will need to put it on a double format since we want to include its decimal point.
+            double average = (double) sum / numbers.length;
+
+            // Final Outputs for the Sum and Average.
+            System.out.println("\nSum of the numbers: " + sum);
+            System.out.println("Average of the numbers: " + average);
+        } catch (InputMismatchException e) {
+            System.out.println("Error: Please enter integers only.");
         }
-
-        // This is for the average, you will need to put it on a double format since we want to include its decimal point.
-        double average = (double) sum / numbers.length;
-
-        // Final Outputs for the Sum and Average.
-        System.out.println("\nSum of the numbers: " + sum);
-        System.out.println("Average of the numbers: " + average);
     }
 }
